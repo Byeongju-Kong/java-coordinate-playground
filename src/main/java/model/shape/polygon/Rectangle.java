db2b5ebbbb0ea01b0ceebf347b0a7586c1581dde
Byeongju-Kong <bjuuuu98@gmail.com>
2021-09-30 15:13:23 +0900

    refactor : 직사각형 유효 검사 수정

--- src/main/java/model/shape/polygon/Rectangle.java
+++ src/main/java/model/shape/polygon/Rectangle.java
@@ -2,30 +2,44 @@ package model.shape.polygon;
 
 import model.shape.Point;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 public class Rectangle implements Polygon {
     private final List<Point> points;
 
     private Rectangle(final String[] points) {
-        validateOverlap(points);
-        this.points = new ArrayList<>();
-        IntStream.range(0, 4)
-                .forEach(index -> this.points.add(Point.create(points[index])));
+        validateQuadrilateral(points);
+        this.points = Arrays.stream(points)
+                .map(Point::create)
+                .collect(Collectors.toUnmodifiableList());
+        validateRightAngle();
     }
 
     public static Rectangle generate(final String[] points) {
         return new Rectangle(points);
     }
 
-    private void validateOverlap(final String[] points) throws IllegalArgumentException {
-        if (Arrays.stream(points).distinct().count() != 4) {
-            throw new IllegalArgumentException("네 점 중에 중복되는 점이 있습니다.");
+    private void validateQuadrilateral(final String[] points) {
+        if (points.length != 4) {
+            throw new IllegalArgumentException("점이 4개가 아닙니다.");
         }
+        if (Arrays.stream(points).distinct().count() != 4) {
+            throw new IllegalArgumentException("점에 중복이 있습니다.");
+        }
+    }
+
+    private void validateRightAngle() {
+        if (!this.points.stream().allMatch(this::hasSameXOrY)) {
+            throw new IllegalArgumentException("네 각이 직각이 아닙니다.");
+        }
+    }
+
+    private boolean hasSameXOrY(final Point standardPoint) {
+        return points.stream()
+                .filter(standardPoint::isNotSameOrDiagonal)
+                .count() == 2;
     }
 
     public double getArea() {
--- src/test/java/model/shape/polygon/RectangleTest.java
+++ src/test/java/model/shape/polygon/RectangleTest.java
@@ -2,16 +2,31 @@ package model.shape.polygon;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 class RectangleTest {
-    @Test
-    @DisplayName("중복된 점이 있으면 예외를 발생시킨다.")
-    void generate_ExceptionByOverlappedPoints() {
-        assertThatIllegalArgumentException().isThrownBy(() -> Rectangle.generate(new String[]{"(1,1)", "(1,1)", "(2,1)", "(2,2)"}))
-                .withMessage("네 점 중에 중복되는 점이 있습니다.");
+    @ParameterizedTest
+    @DisplayName("직사각형을 만들 수 없으면 예외를 발생시킨다.")
+    @MethodSource("provideWrongRectangle")
+    void generate_ExceptionByOverlappedPoints(String[] points, String message) {
+        assertThatIllegalArgumentException().isThrownBy(() -> Rectangle.generate(points))
+                .withMessage(message);
+    }
+
+    private static Stream<Arguments> provideWrongRectangle() {
+        return Stream.of(
+                Arguments.of(new String[]{"(1,1)", "(1,1)", "(2,1)", "(2,2)"}, "점에 중복이 있습니다."),
+                Arguments.of(new String[]{"(1,1)", "(2,1)", "(2,2)"}, "점이 4개가 아닙니다."),
+                Arguments.of(new String[]{"(1,2)", "(1,3)", "(3,1)", "(3,3)"}, "네 각이 직각이 아닙니다."),
+                Arguments.of(new String[]{"(1,1)", "(2,1)", "(2,2)", "(3,2)"}, "네 각이 직각이 아닙니다.")
+        );
     }
 
     @Test
