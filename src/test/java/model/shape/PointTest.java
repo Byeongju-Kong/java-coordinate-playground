5aae88c26857c7d2a6c632d1dcf646afea01fa04
Byeongju-Kong <bjuuuu98@gmail.com>
2021-09-30 15:24:48 +0900

    refactor : Test Source들에 점들을 포함하는 Bracket 추가

--- src/test/java/model/shape/PointTest.java
+++ src/test/java/model/shape/PointTest.java
@@ -14,7 +14,7 @@ import static org.assertj.core.api.Assertions.*;
 class PointTest {
     @ParameterizedTest
     @DisplayName("길이가 1이거나 공백을 포함하는 문자열로 객체를 생성하면 예외를 발생시킨다.")
-    @ValueSource(strings = {"", " ", "1 , 2"})
+    @ValueSource(strings = {"()", "( )", "(1 , 2)"})
     void create_ExceptionByBlank(String wrongCoordinate) {
         assertThatIllegalArgumentException().isThrownBy(() -> Point.create(wrongCoordinate))
                 .withMessage("좌표에 대한 입력에 공백이 있거나 좌표에 대한 정보가 없습니다.");
@@ -23,13 +23,13 @@ class PointTest {
     @Test
     @DisplayName("x와 y를 구분하는 구분자가 없는 문자열로 객체를 생성하면 예외를 발생시킨다.")
     void create_ExceptionByNonHavingDelimiter() {
-        assertThatIllegalArgumentException().isThrownBy(() -> Point.create("12"))
+        assertThatIllegalArgumentException().isThrownBy(() -> Point.create("(12)"))
                 .withMessage("x, y 값을 구분하는 구분자 입력이 없습니다.");
     }
 
     @ParameterizedTest
     @DisplayName("x, y 중 하나의 값에 대한 정보로만 객체를 생성하면 예외를 발생시킨다.")
-    @ValueSource(strings = {"1,", ",2"})
+    @ValueSource(strings = {"(1,)", "(,2)"})
     void create_ExceptionByOnlyOneValue(String wrongCoordinate) {
         assertThatIllegalArgumentException().isThrownBy(() -> Point.create(wrongCoordinate))
                 .withMessage("x, y 값 중 하나만 입력하셨습니다.");
@@ -38,8 +38,8 @@ class PointTest {
     @Test
     @DisplayName("두 점의 x값 차, y값의 차를 반환한다.")
     void getDifference() {
-        Point firstPoint = Point.create("1,1");
-        Point secondPoint = Point.create("2,2");
+        Point firstPoint = Point.create("(1,1)");
+        Point secondPoint = Point.create("(2,2)");
         double expect = 1.414;
         double actual = firstPoint.getDistance(secondPoint);
         assertThat(actual).isEqualTo(expect, offset(0.00099));
@@ -49,7 +49,7 @@ class PointTest {
     @DisplayName("x, y 중 하나의 값만 같은 경우에만 true를 반환한다.")
     @MethodSource("providePointsAndNotSameOrDiagonalExpect")
     void isNotSameOrDiagonal(Point another, boolean expect) {
-        Point standard = Point.create("1,1");
+        Point standard = Point.create("(1,1)");
         boolean actual = standard.hasSameXOrSameY(another);
         assertThat(actual).isEqualTo(expect);
     }
