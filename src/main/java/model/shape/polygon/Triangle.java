99f0db75d316d237a35a0e4b169e77eda879e11d
Byeongju-Kong <bjuuuu98@gmail.com>
2021-10-01 16:59:24 +0900

    feat : Triangle 테스트 및 구현

---
+++ src/main/java/model/shape/polygon/Triangle.java
@@ -0,0 +1,41 @@
+package model.shape.polygon;
+
+import model.shape.Line;
+import model.shape.Point;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.IntStream;
+
+public class Triangle {
+    private final List<Line> lines;
+
+    private Triangle(final List<Point> points) {
+        checkPointsGenerateLine(points);
+        lines = new ArrayList<>();
+        IntStream.range(0, 3)
+                .forEach(index -> lines.add(Line.generate(Arrays.asList(points.get(index), points.get((index+1) % 3)))));
+    }
+
+    public static Triangle generate(final List<Point> points) {
+        return new Triangle(points);
+    }
+
+    private void checkPointsGenerateLine(final List<Point> points) {
+        if (points.stream().anyMatch(point -> doesGenerateLine(point, points))) {
+            throw new IllegalArgumentException("세 점이 선을 이룹니다.");
+        }
+    }
+
+    private boolean doesGenerateLine(final Point standardPoint, final List<Point> points) {
+        return points.stream().allMatch(standardPoint::hasSameX)
+                || points.stream().allMatch(standardPoint::hasSameY);
+    }
+
+    public double getArea() {
+        double s = (lines.get(0).getLength() + lines.get(1).getLength() + lines.get(2).getLength()) / 2;
+        return Math.sqrt(s * (s - lines.get(0).getLength()) * (s - lines.get(1).getLength())
+                * (s - lines.get(2).getLength()));
+    }
+}
