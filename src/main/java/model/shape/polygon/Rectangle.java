f35a0bd6cccff209e236255d6d7566e69377093e
Byeongju-Kong <bjuuuu98@gmail.com>
2021-10-01 15:18:13 +0900

    refactor : 생성자 매개변수 수정 및 중복되는 점 검사 로직 삭제

--- src/main/java/model/shape/polygon/Rectangle.java
+++ src/main/java/model/shape/polygon/Rectangle.java
@@ -2,36 +2,26 @@ package model.shape.polygon;
 
 import model.shape.Point;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Rectangle implements Polygon {
     private final List<Point> points;
 
-    private Rectangle(final String[] points) {
-        validateQuadrilateral(points);
-        this.points = Arrays.stream(points)
-                .map(Point::create)
-                .collect(Collectors.toUnmodifiableList());
-        validateRightAngle();
+    private Rectangle(final List<Point> points) {
+        this.points = points;
+        validate(points);
     }
 
-    public static Rectangle generate(final String[] points) {
+    public static Rectangle generate(final List<Point> points) {
         return new Rectangle(points);
     }
 
-    private void validateQuadrilateral(final String[] points) {
-        if (points.length != 4) {
+    private void validate(final List<Point> points) {
+        if (points.size() != 4) {
             throw new IllegalArgumentException("점이 4개가 아닙니다.");
         }
-        if (Arrays.stream(points).distinct().count() != 4) {
-            throw new IllegalArgumentException("점에 중복이 있습니다.");
-        }
-    }
-
-    private void validateRightAngle() {
-        if (!this.points.stream().allMatch(this::hasSameXOrY)) {
+        if (!points.stream().allMatch(this::hasSameXOrY)) {
             throw new IllegalArgumentException("네 각이 직각이 아닙니다.");
         }
     }
--- src/test/java/model/shape/polygon/RectangleTest.java
+++ src/test/java/model/shape/polygon/RectangleTest.java
@@ -1,11 +1,14 @@
 package model.shape.polygon;
 
+import model.shape.Point;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -15,24 +18,27 @@ class RectangleTest {
     @ParameterizedTest
     @DisplayName("직사각형을 만들 수 없으면 예외를 발생시킨다.")
     @MethodSource("provideWrongPointsAndMessage")
-    void generate_ExceptionByOverlappedPoints(String[] points, String message) {
+    void generate_ExceptionByOverlappedPoints(List<Point> points, String message) {
         assertThatIllegalArgumentException().isThrownBy(() -> Rectangle.generate(points))
                 .withMessage(message);
     }
 
     private static Stream<Arguments> provideWrongPointsAndMessage() {
         return Stream.of(
-                Arguments.of(new String[]{"(1,1)", "(1,1)", "(2,1)", "(2,2)"}, "점에 중복이 있습니다."),
-                Arguments.of(new String[]{"(1,1)", "(2,1)", "(2,2)"}, "점이 4개가 아닙니다."),
-                Arguments.of(new String[]{"(1,2)", "(1,3)", "(3,1)", "(3,3)"}, "네 각이 직각이 아닙니다."),
-                Arguments.of(new String[]{"(1,1)", "(2,1)", "(2,2)", "(3,2)"}, "네 각이 직각이 아닙니다.")
+                Arguments.of(Arrays.asList(Point.create("(1,1)"), Point.create("(1,1)"), Point.create("(2,1)")),
+                        "점이 4개가 아닙니다."),
+                Arguments.of(Arrays.asList(Point.create("(1,2)"), Point.create("(1,3)"), Point.create("(3,1)"), Point.create("(3,3)")),
+                        "네 각이 직각이 아닙니다."),
+                Arguments.of(Arrays.asList(Point.create("(1,1)"), Point.create("(2,1)"), Point.create("(2,2)"), Point.create("(3,2)")),
+                        "네 각이 직각이 아닙니다.")
         );
     }
 
     @Test
     @DisplayName("사각형의 면적을 반환한다.")
     void getArea() {
-        Rectangle rectangle = Rectangle.generate(new String[]{"(1,1)", "(1,3)", "(3,1)", "(3,3)"});
+        Rectangle rectangle = Rectangle.generate(
+                Arrays.asList(Point.create("(1,1)"), Point.create("(1,3)"), Point.create("(3,1)"), Point.create("(3,3)")));
         double expect = 4.0;
         double actual = rectangle.getArea();
         assertThat(actual).isEqualTo(expect);
