da10a8bf90a5f0198396f0472bbe586c309cef4c
Byeongju-Kong <bjuuuu98@gmail.com>
2021-10-06 17:01:07 +0900

    refactor : Rectangle 유효 검사 로직 수정

--- src/main/java/model/shape/polygon/Rectangle.java
+++ src/main/java/model/shape/polygon/Rectangle.java
@@ -4,31 +4,55 @@ import model.shape.Line;
 import model.shape.Point;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.lang.Math.pow;
 
 public class Rectangle implements Shape {
     private final List<Point> points;
 
     private Rectangle(final List<Point> points) throws IllegalArgumentException {
         this.points = points;
-        validate(points);
+        validate();
     }
 
     public static Rectangle generate(final List<Point> points) throws IllegalArgumentException {
         return new Rectangle(points);
     }
 
-    private void validate(final List<Point> points) throws IllegalArgumentException {
-        if (!points.stream().allMatch(this::hasSameXOrY)) {
+    private void validate() throws IllegalArgumentException {
+        double lengthOfDiagonal = findDiagonal();
+        if (!points.stream().allMatch(point -> isRightAngle(point, lengthOfDiagonal))) {
             throw new IllegalArgumentException("네 각이 직각이 아닙니다.");
         }
     }
 
-    private boolean hasSameXOrY(final Point standardPoint) {
-        return points.stream()
-                .filter(point -> point.getXDifference(standardPoint) == 0 || point.getYDifference(standardPoint) == 0)
-                .filter(point -> !(point.getXDifference(standardPoint) == 0 && point.getYDifference(standardPoint) == 0))
-                .count() == 2;
+    private double findDiagonal() {
+        List<Double> lengths = points.stream()
+                .filter(point -> !point.equals(points.get(0)))
+                .map(point -> Line.generate(Arrays.asList(points.get(0), point)).getAttribute())
+                .collect(Collectors.toUnmodifiableList());
+        return Collections.max(lengths);
+    }
+
+    private boolean isRightAngle(final Point standardPoint, final double lengthOfDialog) {
+        double[] lengths = points.stream()
+                .filter(point -> !standardPoint.equals(point))
+                .mapToDouble(point -> Line.generate(Arrays.asList(point, standardPoint)).getAttribute())
+                .filter(length -> length != lengthOfDialog)
+                .toArray();
+        if (isSameBaseLineWithDiagonal(lengths)) {
+            return Math.round((pow(lengths[0], 2) + pow(lengths[0], 2)) * 100) / 100.0
+                    == Math.round(pow(lengthOfDialog, 2) * 100) / 100.0;
+        }
+        return Math.round((pow(lengths[0], 2) + pow(lengths[1], 2)) * 100) / 100.0
+                == Math.round(pow(lengthOfDialog, 2) * 100) / 100.0;
+    }
+
+    private boolean isSameBaseLineWithDiagonal(double[] lengths) {
+        return lengths.length == 1;
     }
 
     @Override
