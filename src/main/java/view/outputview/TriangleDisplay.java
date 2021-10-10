473e7232ad47f3f18c7c4b441f959407628d1099
Byeongju-Kong <bjuuuu98@gmail.com>
2021-10-10 15:53:43 +0900

    refactor : printPoint, enterNewLine 메소드 추가

--- src/main/java/view/outputview/TriangleDisplay.java
+++ src/main/java/view/outputview/TriangleDisplay.java
@@ -1,6 +1,9 @@
 package view.outputview;
 
-public class TriangleDisplay implements OutputDisplay  {
+public class TriangleDisplay implements OutputDisplay {
+    private static final String POINT_SYMBOL = "*";
+    private static final String EMPTY = "  ";
+
     @Override
     public boolean hasAppropriateNumber(int numberOfPoints) {
         return numberOfPoints == 3;
@@ -9,5 +12,20 @@ public class TriangleDisplay implements OutputDisplay  {
     @Override
     public void showAttribute(double attribute) {
         System.out.println("삼각형의 넓이는 " + attribute);
+    }
+
+    @Override
+    public void printPoint(boolean correctPoint) {
+        if (correctPoint) {
+            System.out.print(POINT_SYMBOL);
+        }
+        if (!correctPoint) {
+            System.out.print(EMPTY);
+        }
+    }
+
+    @Override
+    public void enterNewLine() {
+        System.out.println();
     }
 }
