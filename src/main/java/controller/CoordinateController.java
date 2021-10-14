12fc9bfef88c0213da87163b7d77604e3a9fa6c4
Byeongju-Kong <bjuuuu98@gmail.com>
2021-10-14 14:12:38 +0900

    refactor : ShapeGenerator 적용

--- src/main/java/controller/CoordinateController.java
+++ src/main/java/controller/CoordinateController.java
@@ -1,47 +1,20 @@
 package controller;
 
-import model.dto.InputDTO;
-import model.shape.Shapes;
 import model.shape.polygon.Shape;
 import view.inputview.InputDisplay;
 import view.outputview.OutputDisplay;
 import view.outputview.OutputDisplays;
 
 public class CoordinateController {
-    private Shape shape;
-    private final InputDisplay inputDisplay;
-    private OutputDisplay outputDisplay;
-    private InputDTO inputDTO;
-    private boolean inputPointsGenerateWrongShape;
+    private final ShapeGenerator shapeGenerator;
 
     public CoordinateController(InputDisplay inputDisplay) {
-        this.inputDisplay = inputDisplay;
-        inputPointsGenerateWrongShape = true;
+        shapeGenerator = new ShapeGenerator(inputDisplay);
     }
 
     public void run() {
-        while (inputPointsGenerateWrongShape) {
-            generateShape();
-        }
-        findOutputDisplay();
+        Shape shape = shapeGenerator.generateShape();
+        OutputDisplay outputDisplay = OutputDisplays.findOutputView(shapeGenerator.getNumberOfPoints());
         outputDisplay.showAttribute(shape.getAttribute());
-    }
-
-    private void generateShape() {
-        try {
-            inputPoints();
-        } catch (IllegalArgumentException exception) {
-            inputDisplay.showExceptionMessage(exception.getMessage());
-        }
-    }
-
-    private void inputPoints() throws IllegalArgumentException {
-        inputDTO = InputDTO.create(inputDisplay.inputPoints());
-        shape = Shapes.findShape(inputDTO.getInputPoints());
-        inputPointsGenerateWrongShape = false;
-    }
-
-    private void findOutputDisplay() {
-        outputDisplay = OutputDisplays.findOutputView(inputDTO.getNumberOfPoints());
     }
 }
