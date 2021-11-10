435b0baa2e5d37e5280796a0375e7fcbd487bf1f
Byeongju-Kong <bjuuuu98@gmail.com>
2021-11-10 20:53:23 +0900

    refactor : 클래스 검사 방법 수정

--- src/test/java/model/shape/ShapesTest.java
+++ src/test/java/model/shape/ShapesTest.java
@@ -1,5 +1,7 @@
 package model.shape;
 
+import model.shape.polygon.Rectangle;
+import model.shape.polygon.Triangle;
 import model.vo.Point;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -15,20 +17,20 @@ import static org.assertj.core.api.Assertions.assertThat;
 class ShapesTest {
     @ParameterizedTest
     @DisplayName("점의 개수에 따라 적절한 도형을 반환한다.")
-    @MethodSource("providePointsAndShape")
-    void findShape(List<Point> points, String expect) {
-        String actual = Shapes.findShape(points).getClass().getSimpleName();
+    @MethodSource("providePointsAndShapeClass")
+    void findShape(List<Point> points, Object expect) {
+        Object actual = Shapes.findShape(points).getClass();
         assertThat(actual).isEqualTo(expect);
     }
 
-    private static Stream<Arguments> providePointsAndShape() {
+    private static Stream<Arguments> providePointsAndShapeClass() {
         return Stream.of(
                 Arguments.of(Arrays.asList(Point.create("(1,1)"), Point.create("(1,2)")),
-                        "Line"),
+                        Line.class),
                 Arguments.of(Arrays.asList(Point.create("(5,1)"), Point.create("(1,2)"), Point.create("(2,2)")),
-                        "Triangle"),
+                        Triangle.class),
                 Arguments.of(Arrays.asList(Point.create("(1,1)"), Point.create("(1,2)"), Point.create("(2,2)"), Point.create("(2,1)")),
-                        "Rectangle")
+                        Rectangle.class)
         );
     }
 }
